f19dc82bdd74892c8d0010deda995b6f8870cc59
Chris Kypridemos <ckyprid@liverpool.ac.uk>
2022-04-10 15:53:13 +0100

    do not track excel files

--- gh_upload.R
+++ gh_upload.R
@@ -2,7 +2,7 @@ library(piggyback)
 tag <- "v0.0.1"
 repo <- "ChristK/IMPACTncd_Engl"
 # pb_new_release(repo, tag)
-fl <- list.files(getwd(), pattern = ".fst$", full.names = TRUE, recursive = TRUE)
+fl <- list.files(getwd(), pattern = ".fst$|.xls$|.xlsx$", full.names = TRUE, recursive = TRUE)
 fl <- grep("secure_data", fl, value = TRUE, invert = TRUE)
 pb_track(fl)
 
