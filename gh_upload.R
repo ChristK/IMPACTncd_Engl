cb43a0dfaedef628aabbfa91040774e76db5e37d
Chris Kypridemos <ckyprid@liverpool.ac.uk>
2022-04-21 10:53:03 +0100

    after accidental deletion

--- gh_upload.R
+++ gh_upload.R
@@ -1,9 +1,14 @@
 library(piggyback)
-tag <- "v0.0.1"
+tag <- "v0.0.2"
 repo <- "ChristK/IMPACTncd_Engl"
 # pb_new_release(repo, tag) # Only need to run the first time a github repo is created
 fl <- list.files(getwd(), pattern = ".fst$|.xls$|.xlsx$.qs$", full.names = TRUE, recursive = TRUE)
 fl <- grep("secure_data", fl, value = TRUE, invert = TRUE)
+
+# pb_delete(file = fl,
+#           repo = repo,
+#           tag = tag)
+
 pb_upload(fl, repo = repo, tag = tag)
 
 pb_list(repo = repo, tag = tag)
