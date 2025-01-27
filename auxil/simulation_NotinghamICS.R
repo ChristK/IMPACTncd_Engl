b856cd8a8ef51d2cebae76a8ca9b60272ecb369a
Chris Kypridemos <ckyprid@liverpool.ac.uk>
2025-01-27 18:34:36 +0000

    Add simulation script for Nottingham ICS and update documentation for population weight adjustment

---
+++ auxil/simulation_NotinghamICS.R
@@ -0,0 +1,16 @@
+source("global.R")
+
+IMPACTncd <- Simulation$new("./inputs/sim_design_NotinghamICS.yaml")
+
+
+n_runs <- 100L
+
+# IMPACTncd$
+#   del_logs()$
+#   del_outputs()$
+#   run(1:n_runs, multicore = TRUE, "sc0")$
+#   export_summaries(multicore = TRUE)
+
+IMPACTncd$export_summaries(multicore = TRUE)
+source("./auxil/process_out_for_NotinghamICS.R")
+
