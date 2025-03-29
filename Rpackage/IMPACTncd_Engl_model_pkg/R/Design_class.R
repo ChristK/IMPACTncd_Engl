ea4f2a128c2f45848f0036a5c16e5ef9410524d1
Chris Kypridemos <ckyprid@liverpool.ac.uk>
2025-03-29 22:21:38 +0000

    Add unit tests and update package metadata for IMPACTncdEngl

--- Rpackage/IMPACTncd_Engl_model_pkg/R/Design_class.R
+++ Rpackage/IMPACTncd_Engl_model_pkg/R/Design_class.R
@@ -20,14 +20,53 @@
 ## Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 
-#' R6 Class representing a simulation design
+#' @title Design Class
+#' @name Design
 #'
 #' @description
-#' A design has a sim_prm list that holds the simulation parameters.
-#' This R6 class represents a simulation design with associated parameters and methods.
+#' An R6 class representing a simulation design for the IMPACTncd framework.
+#' This class encapsulates simulation configuration parameters (`sim_prm`),
+#' enforces validation, supports GUI updates, and provides tools for detecting and resolving
+#' dependency structures among diseases.
 #'
 #' @details
-#' To be completed...
+#' The `Design` class is used to initialize and manage the structure of a simulation run.
+#' It can load simulation parameters from a YAML configuration file or directly from an R list.
+#' The class performs validation of required fields, sets default values, and computes derived fields
+#' like initial year and simulation horizon. It also handles the topological ordering of diseases
+#' based on their dependencies and can detect cyclic relationships using `igraph`.
+#'
+#' @section Initialization:
+#' The class is initialized with either a file path (YAML) or a list:
+#'
+#' \code{
+#' design <- Design$new("inputs/sim_design.yaml")
+#' }
+#'
+#' @section Public Fields:
+#' \describe{
+#'   \item{\code{sim_prm}}{A named list of validated and processed simulation parameters.}
+#' }
+#'
+#' @section Public Methods:
+#' \describe{
+#'   \item{\code{initialize(sim_prm)}}{Initializes the object. Accepts a list or path to YAML.}
+#'   \item{\code{save_to_disk(path)}}{Saves the current configuration to disk as a YAML file.}
+#'   \item{\code{update_fromGUI(GUI_prm)}}{Updates the design based on GUI-provided inputs.}
+#'   \item{\code{print()}}{Prints the current design parameters.}
+#' }
+#'
+#' @section Private Methods:
+#' \describe{
+#'   \item{\code{detect_cycles(sim_prm)}}{Detects feedback loops in disease dependencies.}
+#'   \item{\code{reorder_diseases(sim_prm)}}{Sorts diseases in topological order based on their dependencies.}
+#' }
+#'
+#' @importFrom R6 R6Class
+#' @importFrom yaml read_yaml write_yaml
+#' @importFrom igraph make_graph V neighbors all_simple_paths topo_sort
+#'
+#' @seealso \code{\link[yaml]{read_yaml}}, \code{\link[igraph]{make_graph}}
 #'
 #' @export
 Design <-
@@ -44,12 +83,15 @@ Design <-
       #'   appropriate format.
       #' @return A new `Design` object.
       #' @examples
-      #' design <- Design$new("./validation/design_for_trends_validation.yaml")
+      #' design <- Design$new("inputs/sim_design.yaml")
+      # initialize ----
       initialize = function(sim_prm) {
+        # Determine the type of the input parameter
         data_type <- typeof(sim_prm)
+        
+        # Read YAML file if the input is a character string
         if (data_type == "character") {
           sim_prm <- read_yaml(base::normalizePath(sim_prm, mustWork = TRUE))
-
         } else if (data_type != "list") {
           stop(
             "You can initialise the object only with an R object of
@@ -57,120 +99,99 @@ Design <-
           )
         }
 
-        # Validation
+        # Validate simulation parameters
+        required_fields <- c(
+          "simulation_files_overwrite",
+          "sTag",
+          "bOverwriteFilesOnDeploy",
+          "RootDirPath",
+          "sToken",
+          "locality",
+          "clusternumber",
+          "logs",
+          "scenarios",
+          "cols_for_output",
+          "strata_for_output",
+          "exposures",
+          "n",
+          "init_year_long",
+          "sim_horizon_max",
+          "ageL",
+          "ageH",
+          "diseases",
+          "maxlag",
+          "smoking_relapse_limit",
+          "stochastic",
+          "kismet",
+          "jumpiness",
+          "statin_adherence",
+          "bpmed_adherence",
+          "decision_aid",
+          "export_xps",
+          "simsmok_calibration",
+          "output_dir",
+          "synthpop_dir",
+          "validation",
+          "iteration_n_max",
+          "n_synthpop_aggregation"
+        )
+        
         stopifnot(
-          c(
-            "simulation_files_overwrite",
-            "sTag"                   ,
-            "bOverwriteFilesOnDeploy",
-            "RootDirPath"  ,
-            "sToken"                ,
-            "locality"              ,
-            "clusternumber"         ,
-            "logs"                  ,
-            "scenarios"             ,
-            "cols_for_output"       ,
-            "strata_for_output"     ,
-            "exposures"             ,
-            "n"                     ,
-            "init_year_long"        ,
-            "sim_horizon_max"       ,
-            "ageL"                  ,
-            "ageH"                  ,
-            "diseases"              ,
-            "maxlag"                ,
-            "smoking_relapse_limit" ,
-            "stochastic"            ,
-            "kismet"                ,
-            "jumpiness"             ,
-            "statin_adherence"      ,
-            "bpmed_adherence"       ,
-            "decision_aid"          ,
-            "export_xps"            ,
-            "simsmok_calibration"   ,
-            "output_dir"            ,
-            "synthpop_dir"          ,
-            "validation"            ,
-            "iteration_n_max"       ,
-            "n_synthpop_aggregation"
-          ) %in% names(sim_prm),
-
-          sapply(sim_prm, function(x)
-            if (is.numeric(x))
-              x >= 0
-            else
-              TRUE)
+          required_fields %in% names(sim_prm),
+          sapply(sim_prm, function(x) if (is.numeric(x)) x >= 0 else TRUE)
         )
 
-
+        # Setup simulation parameters with default values and adjustments
         sim_prm$sim_horizon_max <- sim_prm$sim_horizon_max - sim_prm$init_year_long
         sim_prm$init_year <- sim_prm$init_year_long - 2000L
-        # place holders to be updated from self$update_fromGUI(parameters)
-
+        
+        # Default values
         sim_prm$national_qimd       <- TRUE
         sim_prm$init_year_fromGUI   <- sim_prm$init_year
         sim_prm$sim_horizon_fromGUI <- sim_prm$sim_horizon_max
 
-        # Force LAD population proj if smaller localities
-        if (!"England" %in% sim_prm$locality) sim_prm$calibrate_to_pop_projections_by_LAD <- TRUE
+        # Force LAD population projection if smaller localities are used
+        if (!"England" %in% sim_prm$locality) {
+          sim_prm$calibrate_to_pop_projections_by_LAD <- TRUE
+        }
 
-        # Create synthpop_dir_ if it doesn't exists
+        # Normalize output and directory path
         sim_prm$output_dir <-
           normalizePath(sim_prm$output_dir, mustWork = FALSE)
+        sim_prm$synthpop_dir <-
+          normalizePath(sim_prm$synthpop_dir, mustWork = FALSE)
 
+        # Reorder the diseases based on dependencies
+        self$reorder_diseases(sim_prm)
 
-        # Reorder the diseases so dependencies are always calculated first
-        # (topological ordering). This is crucial for init_prevalence
-        # first name the list and
-        sim_prm$diseases <-
-          setNames(sim_prm$diseases, sapply(sim_prm$diseases, function(x)
-            x$name))
-
-        out <- vector() # will hold graph structure
-        ds <- names(sim_prm$diseases)
-        for (i in seq_along(ds)) {
-          ds_ <- ds[i]
-          dep <-
-            sim_prm[["diseases"]][[i]][["meta"]][["incidence"]][["influenced_by_disease_name"]]
-          if (length(dep) > 0L) {
-            # dep <- gsub("_prvl", "", dep)
-            for (j in seq_along(dep)) {
-              out <- c(out, dep[[j]], ds_)
-            }
-          }
+        # Detect cycles in the disease graph
+        cycles <- self$detect_cycles(sim_prm)
+        if (sim_prm$logs && length(cycles) > 0) {
+          message("Cycles found: ", cycles)
         }
-        g <- make_graph(out, directed = TRUE)
-        # stopifnot(is_dag(g)) # Removed for diseases depend on diseases
-        # get all cycles in the graph
-        Cycles = NULL
-        for(v1 in V(g)) {
-          for(v2 in neighbors(g, v1, mode="out")) {
-            Cycles = c(Cycles,
-                       lapply(all_simple_paths(g, v2,v1, mode = "out"), function(p) c(v1,p)))
-          }
-        }
-        # remove duplicates
-        Cycles <- Cycles[sapply(Cycles, min) == sapply(Cycles, `[`, 1)]
-        # find cycles of length i.e. 3 (i.e. chd -> t2dm -> chd)
-        Cycles[which(sapply(Cycles, length) >= 3)]
 
-        if (sim_prm$logs && length(Cycles) > 0) message("Cycles found: ", Cycles)
-
-        o <- topo_sort(g)
-
-        # then reorder based on the topological ordering
-        sim_prm$diseases <- sim_prm$diseases[order(match(names(sim_prm$diseases), names(o)))]
-
-
+        # Store the simulation parameters
         self$sim_prm = sim_prm
 
         invisible(self)
       },
 
-      #' @description Create a new design object.
-      #' @param path Path including file name and extension to save a yaml
-      #'   file with the simulation parameters.
-      #' @return The `Design` object.
+      #' Save Simulation Design to Disk
+      #' @description
+      #' Serializes the current simulation parameters (`sim_prm`) and writes them to a YAML file.
+      #' This can be useful for saving configurations that can later be reloaded or shared.
+      #'
+      #' @param path A character string specifying the full file path (including filename and extension)
+      #' where the simulation design should be saved.
+      #'
+      #' @return The `Design` object, invisibly.
+      #'
+      #' @examples
+      #' design <- Design$new("inputs/sim_design.yaml")
+      #' design$save_to_disk("outputs/saved_sim_design.yaml")
+      #'
+      #' @importFrom yaml write_yaml
+      # save_to_disk ----
       save_to_disk = function(path) {
         write_yaml(self$sim_prm, base::normalizePath(path, mustWork = FALSE))
 
@@ -180,6 +201,7 @@ Design <-
       #' @description Updates the design object from GUI.
       #' @param GUI_prm A GUI parameter object.
       #' @return The `Design` object.
+      # update_fromGUI ----
       update_fromGUI = function(GUI_prm) {
         self$sim_prm$national_qimd       <- GUI_prm$national_qimd_checkbox
         # T = use national qimd, F = use local qimd
@@ -188,10 +210,12 @@ Design <-
         self$sim_prm$sim_horizon_fromGUI <-
           fromGUI_timeframe(GUI_prm)["horizon"]
         self$sim_prm$locality <- GUI_prm$locality_select
+        
         if (!GUI_prm$national_qimd_checkbox) {
           self$sim_prm$cols_for_output <-
             c(setdiff(self$sim_prm$cols_for_output, "lqimd"), "nqimd")
         }
+        
         self$sim_prm$iteration_n            <- GUI_prm$iteration_n_gui
         self$sim_prm$iteration_n_final      <- GUI_prm$iteration_n_final_gui
         self$sim_prm$n                      <- GUI_prm$n_gui
@@ -210,6 +234,7 @@ Design <-
       #' @description
       #' Print the simulation parameters.
       #' @return The `Design` object.
+      # print ----
       print = function() {
         print(self$sim_prm)
 
@@ -218,7 +243,81 @@ Design <-
     ),
 
     # private ------------------------------------------------------------------
-     private = list(
-      mc_aggr = NA
+    private = list(
+      mc_aggr = NA,
+
+      
+      # Detect Cycles in Disease Dependency Graph
+      # @description
+      # Constructs a directed graph based on disease dependencies and searches for cycles.
+      # This is used to detect feedback loops in the disease model, which may indicate modeling errors
+      # or complex feedback needing special handling.
+      # 
+      # @param sim_prm A list of simulation parameters that includes a named list of diseases,
+      # where each disease may list other diseases that influence its incidence.
+      # 
+      # @return A list of cycles found, where each cycle is a sequence of disease names involved in a dependency loop.
+      # If no cycles are found, an empty list is returned.
+      # 
+      # @importFrom igraph make_graph V neighbors all_simple_paths
+      # @keywords internal
+      # detect_cycles ----
+      detect_cycles = function(sim_prm) {
+        out <- vector() # will hold graph structure
+        ds <- names(sim_prm$diseases)
+        
+        for (i in seq_along(ds)) {
+          ds_ <- ds[i]
+          dep <-
+            sim_prm[["diseases"]][[i]][["meta"]][["incidence"]][["influenced_by_disease_name"]]
+          if (length(dep) > 0L) {
+            for (j in seq_along(dep)) {
+              out <- c(out, dep[[j]], ds_)
+            }
+          }
+        }
+        
+        g <- make_graph(out, directed = TRUE)
+        Cycles = NULL
+        for(v1 in V(g)) {
+          for(v2 in neighbors(g, v1, mode="out")) {
+            Cycles = c(Cycles,
+                       lapply(all_simple_paths(g, v2,v1, mode = "out"), function(p) c(v1,p)))
+          }
+        }
+        # remove duplicates
+        Cycles <- Cycles[sapply(Cycles, min) == sapply(Cycles, `[`, 1)]
+        # find cycles of length >= 3
+        return(Cycles[which(sapply(Cycles, length) >= 3)])
+      },
+
+      # Reorder Diseases by Dependency
+      # @description
+      # Reorders the list of diseases in the simulation parameters using topological sorting.
+      # This ensures that diseases are processed in the correct order based on their causal dependencies,
+      # such that diseases influencing others appear earlier in the list.
+      # 
+      # This is critical for simulations that depend on proper initialization and evaluation
+      # of disease incidence affected by the presence of other diseases.
+      #
+      # @param sim_prm A list of simulation parameters. Must contain a named list of diseases
+      # where each disease has a 'name' field and may have dependencies listed under
+      # meta$incidence$influenced_by_disease_name.
+      #
+      # @return None. The function updates `sim_prm$diseases` in place to reflect the dependency order.
+      # 
+      # @importFrom igraph make_graph topo_sort
+      # @keywords internal
+      # reorder_diseases ----
+      reorder_diseases = function(sim_prm) {
+        # first name the list
+        sim_prm$diseases <-
+          setNames(sim_prm$diseases, sapply(sim_prm$diseases, function(x)
+            x$name))
+
+        o <- topo_sort(make_graph(unlist(sim_prm$diseases)))
+        # then reorder based on the topological ordering
+        sim_prm$diseases <- sim_prm$diseases[order(match(names(sim_prm$diseases), names(o)))]
+      }
     )
   )
